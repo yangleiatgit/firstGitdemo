b75ec6021f4fcda03d9708da125c8c73dad51924
yangleiatgit2 <476379874@qq.com>
2019-09-15 00:14:20 +0800

    提交到master 分支

--- child-war/src/main/java/com/bjsxt/mapper/UsersMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bjsxt.mapper;
-
-import java.util.List;
-
-import com.bjsxt.pojo.Users;
-
-public interface UsersMapper {
-	List<Users> selAll();
-}
--- child-war/src/main/java/com/bjsxt/service/UsersService.java
+++
@@ -1,9 +0,0 @@
-package com.bjsxt.service;
-
-import java.util.List;
-
-import com.bjsxt.pojo.Users;
-
-public interface UsersService {
-	List<Users> show();
-}
